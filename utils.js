331903905ccb70f0cb6d85c056f4c3820f1fd962
Jonathan Higgins <jonathan.higgins1@marist.edu>
2015-02-24 18:51:23 -0500

    added cases and updated utils.js
    i added the rest of the cases and i added the function whiteSpace trim
    to get rid of all the whitespace

--- utils.js
+++ utils.js
@@ -3,3 +3,16 @@
 	utility functions
 	\\\\\\\\\\\\\\\\\
 */
+
+function whiteSpaceTrim(str)
+//a regular expression to get rip of whitespaces
+spaces. {
+			return str.replace(/^\s+ | \s+$/g, "");
+
+			// "/^\s+" represents whitespace characters before the string
+			// "|" seperates this into two expressions.
+			// "\s+$/" represents the whitespace characters that follow the string
+			// "g" makes this global so that it it will get all the whitespace
+			// "" this represents nothing, which is what replaced the whitespace
+			
+}
