1adadd0f8079728de813772d52f4722d5adc9c51
Jonathan Higgins <jonathan.higgins1@marist.edu>
2015-02-27 00:28:29 -0500

    all of my parse functions for our grammar
    this commit is all of te parse functions for our grammar

--- utils.js
+++ utils.js
@@ -4,9 +4,8 @@
 	\\\\\\\\\\\\\\\\\
 */
 
-function whiteSpaceTrim(str)
-//a regular expression to get rip of whitespaces
-spaces. {
+function whiteSpaceTrim(str) {
+//a regular expression to get rip of whitespacesspaces. 
 			return str.replace(/^\s+ | \s+$/g, "");
 
 			// "/^\s+" represents whitespace characters before the string
@@ -14,5 +13,5 @@ spaces. {
 			// "\s+$/" represents the whitespace characters that follow the string
 			// "g" makes this global so that it it will get all the whitespace
 			// "" this represents nothing, which is what replaced the whitespace
-			
+			 
 }
